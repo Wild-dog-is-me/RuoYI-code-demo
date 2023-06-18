781e3bf51fad77bfbcef51ef3187b50bc2e0b985
Wild-dog-is-me <1427774041@qq.com>
2023-06-18 21:22:48 +0800

    异常捕获bug修复

--- Seata_AT/storage/src/main/java/org/dog/storage/StorageApplication.java
+++ Seata_AT/storage/src/main/java/org/dog/storage/StorageApplication.java
@@ -3,7 +3,7 @@ package org.dog.storage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "org.dog")
 public class StorageApplication {
 
     public static void main(String[] args) {
